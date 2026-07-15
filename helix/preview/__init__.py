204a14382673585241797340586f80738218e20b
ryankorkowski-boop <ryankorkowski@gmail.com>
2026-07-15 06:02:40 -0500

    Export animation and rendering preview components

--- helix/preview/__init__.py
+++ helix/preview/__init__.py
@@ -5,5 +5,12 @@ Event-driven rendering preview for Helix sequencing output.
 
 from .renderer import FrameRenderer
 from .timeline import Timeline
+from .animation import AnimationEngine
+from .pillow_renderer import PillowRenderer
 
-__all__ = ["FrameRenderer", "Timeline"]
+__all__ = [
+    "FrameRenderer",
+    "Timeline",
+    "AnimationEngine",
+    "PillowRenderer",
+]
